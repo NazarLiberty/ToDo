f7dd420d5613547d1c7ca63898190111bdfeed13
NazarLiberty <NazarLiberty@gmail.com>
2020-05-12 17:28:22 +0300

    Initial commit

---
+++ precache-manifest.6df726bc6b42be0fc4ca1635870fbc07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7437e1c9af79ba94debf4e0fe7a497d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddea515f248679fe3aec",
+    "url": "/static/css/main.09f1f5e0.chunk.css"
+  },
+  {
+    "revision": "fc70003b9b872067d527",
+    "url": "/static/js/2.180a3c7a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.180a3c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddea515f248679fe3aec",
+    "url": "/static/js/main.807de4d9.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
